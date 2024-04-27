fdef3c1090939c7882ab87e055995551493788c3
Prash1723 <s.praveen795@gmail.com>
2024-04-27 23:53:32 +0530

    Logging module to better control error

--- Prices/app.py
+++ Prices/app.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import time
 import talib
+import logging
 
 from rich.panel import Panel
 from rich.console import Console
@@ -21,60 +22,89 @@ int_coins = {"bicoin": "BTC-INR", "etherium": "ETH-INR"}
 
 currency = {"Indian Rupees": "INR=X", "Japanese Yen": "JPY=X"}
 
+# Configuration for logging
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+# Assign logger
+log = logging.getlogger('rich')
+
+# File output settings
+FileOut = logging.FileHandler("app.log")
+
+log.addHandler(FileOut)
+
 def generate_protable():
     """Generates the prices of securities available in the market"""
-    table = Table(title="Securities Price", style='bold yellow')
-    table.add_column("Security")
-    table.add_column("Current Price")
-    
-    tab1 = table
+    try:
+        table = Table(title="Securities Price", style='bold yellow')
+        table.add_column("Security")
+        table.add_column("Current Price")
+        
+        tab1 = table
 
-    for k,v in products.items():
-        product = yf.Ticker(v)
-        price = product.info['previousClose']
-        table.add_row(f"{k}", f"{price}")
+        for k,v in products.items():
+            product = yf.Ticker(v)
+            price = product.info['previousClose']
+            table.add_row(f"{k}", f"{price}")
+
+    except Exception as e:
+        log.error(f"Error: {e}")
 
     return table
 
 def generate_orgtable():
     """Generates the current share price of organizations"""
-    table = Table(title="Share Price", style="bold green")
-    table.add_column("Share")
-    table.add_column("Current Price")
+    try:
+        table = Table(title="Share Price", style="bold green")
+        table.add_column("Share")
+        table.add_column("Current Price")
 
-    for k,v in organisations.items():
-        product = yf.Ticker(v)
-        price = product.info['currentPrice']
+        for k,v in organisations.items():
+            product = yf.Ticker(v)
+            price = product.info['currentPrice']
 
-        table.add_row(f"{k}", f"{price}")
+            table.add_row(f"{k}", f"{price}")
+
+    except Exception as e:
+        log.errot(f"Error: {e}")
 
     return table
 
 def generate_coin():
     """Generates the current crypto currency to Indian rupees exchange rate"""
-    table = Table(title="Coin Price", style="bold red")
-    table.add_column("Coin")
-    table.add_column("Exchange rate")
+    try:
+        table = Table(title="Coin Price", style="bold red")
+        table.add_column("Coin")
+        table.add_column("Exchange rate")
 
-    for k,v in int_coins.items():
-        product = yf.Ticker(v)
-        price = product.info['previousClose']
+        for k,v in int_coins.items():
+            product = yf.Ticker(v)
+            price = product.info['previousClose']
 
-        table.add_row(f"{k}", f"{price}")
+            table.add_row(f"{k}", f"{price}")
+
+        except Exception as e:
+            log.errot(f"Error: {e}")
 
     return table
 
 def generate_currency():
     """Generates the current exchange rate of currencies to US Dollars"""
-    table = Table(title="Currency compared to USD", style="bold blue")
-    table.add_column("Currency")
-    table.add_column("Exchange rate")
+    try:
+        table = Table(title="Currency compared to USD", style="bold blue")
+        table.add_column("Currency")
+        table.add_column("Exchange rate")
 
-    for k,v in currency.items():
-        product = yf.Ticker(v)
-        price = product.info['previousClose']
+        for k,v in currency.items():
+            product = yf.Ticker(v)
+            price = product.info['previousClose']
 
-        table.add_row(f"{k}", f"{price}")
+            table.add_row(f"{k}", f"{price}")
+
+    except Exception as e:
+        log.errot(f"Error: {e}")
 
     return table
 
